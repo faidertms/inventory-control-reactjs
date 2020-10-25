6d226199c2be4d9fc646775fbc1d2cbf46664743
thiago sales <thiagomsales@hotmail.com>
2020-10-25 13:28:19 -0300

    Correção de tipagem e remoção de uma pagina

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import axiosRetry from 'axios-retry';
 
-export const axiosApi = axios.create({
+export const axiosApi: AxiosInstance = axios.create({
     baseURL: 'http://127.0.0.1:8000/api',
     timeout: 240000,
     headers:
